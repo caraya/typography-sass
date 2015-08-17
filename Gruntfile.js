16a90895dc61faa6798a2f84acc2a964c3bf4615
Carlos Araya <carlos.araya@gmail.com>
2015-08-16 22:33:18 -0700

    setting up tooling

---
+++ Gruntfile.js
@@ -0,0 +1,136 @@
+/*global module */
+/*global require */
+(function () {
+  'use strict';
+  module.exports = function (grunt) {
+    // require time-grunt at the top and pass in the grunt instance
+    // it will measure how long things take for performance
+    //testing
+    require('time-grunt')(grunt);
+    // load-grunt will read the package file and automatically
+    // load all our packages configured there.
+    // Yay for laziness
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+      // SASS RELATED TASKS
+      // Converts all the files under scss/ ending with .scss
+      // into the equivalent css file on the css/ directory
+      sass: {
+        dev: {
+          options: {
+            style: 'expanded'
+          },
+          files: [ {
+            expand: true,
+            cwd: '.',
+            src: [ '**/*.scss'],
+            dest: 'css',
+            ext: '.css'
+          }]
+        },
+        production: {
+          options: {
+            style: 'compact'
+          },
+          files: [ {
+            expand: true,
+            cwd: 'sass',
+            src: [ '**/*.scss'],
+            dest: 'css',
+            ext: '.css'
+          }]
+        }
+      },
+
+      // This task requires the scss-lint ruby gem to be
+      // installed on your system If you choose not to
+      // install it, comment out this task and the prep-css
+      // and work-lint tasks below
+      //
+      // I've chosen not to fail on errors or warnings.
+      scsslint: {
+        allFiles: [ '**/*.scss' ],
+        options: {
+          force: true,
+          colorizeOutput: true
+        }
+      },
+
+      // GH-PAGES TASK
+      // Push the specified content into the repositories
+      // gh-pages branch
+      'gh-pages': {
+        options: {
+          message: 'Content committed from Grunt gh-pages',
+          base: 'sassdoc',
+          dotfiles: true
+        },
+        // These files will get pushed to the `
+        // gh-pages` branch (the default)
+        src: ['**/*']
+      },
+
+      // OPTIONAL TASKS //
+
+      // Autoprefixer will check caniuse.com's database and
+      // add the necessary prefixes to CSS elements as needed.
+      // This saves us from doing the work manually
+      autoprefixer: {
+        options: {
+          browsers: [ 'last 2 versions', 'ie8', 'ie9' ]
+        },
+
+        files: {
+          expand: true,
+          flatten: true,
+          src: 'css/*.css',
+          dest: 'css/*.css'
+        }
+      },
+
+      // UNCSS will analyzes the your HTML pages and
+      // remove from the CSS all the classes that are
+      // not used in any of your HTML pages
+      //
+      // This task needs to be run in the processed CSS
+      // rather than the SCSS files
+      //
+      //See https://github.com/addyosmani/grunt-uncss
+      // for more information
+      uncss: {
+        dist: {
+          files: {
+            'dist/css/main.css': [ 'dist/*.html' ]
+          }
+        }
+      },
+
+
+    });
+    // closes initConfig
+
+    // CUSTOM TASKS
+    // Usually a combination of one or more tasks defined above
+
+    grunt.task.registerTask(
+      'lint',
+      [ 'scsslint' ]
+    )
+
+    grunt.task.registerTask(
+      'process',
+      [ 'scsslint', 'sass:dev', 'autoprefixer' ]
+    );
+
+    grunt.task.registerTask(
+      'post-process',
+      [ 'autoprefixer', 'uncss' ]
+    );
+
+  };
+  // closes module.exports
+}
+());
+// closes the use strict function
